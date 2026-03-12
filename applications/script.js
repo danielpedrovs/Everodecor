7509040af81cb8c201236c589478cbfc3a4186a2
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2026-03-12 16:18:45 +0000

    Update script.js

--- applications/script.js
+++ applications/script.js
@@ -1,1 +1,15 @@
+const openAI = document.getElementById("openAI");
+const chatbox = document.getElementById("chatbox");
+
+if(openAI){
+openAI.addEventListener("click", () => {
+
+if(chatbox.style.display === "block"){
+chatbox.style.display = "none";
+}else{
+chatbox.style.display = "block";
+}
+
+});
+}
 
